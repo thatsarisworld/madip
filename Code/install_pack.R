58cae202c3c57a8513c113f381b793ba35c52008
Arina <arinaturonchik@gmail.com>
2024-05-15 22:01:39 +0200

    initial commit

---
+++ Code/install_pack.R
@@ -0,0 +1,25 @@
+source("Code/my_functions.R")
+
+install.packages(c("plyr", "ggplot2", "highcharter", "sp", "dplyr", 
+                   "lubridate", "rmarkdown", "tidyr", "nycflights13",  
+                   "ggthemes", "xts", "cluster", "plotly", "maps"))
+
+is.installed("plyr")
+is.installed("ggplot2")
+is.installed("highcharter")
+is.installed("dplyr")
+is.installed("lubridate")
+is.installed("stringi")
+is.installed("tidyr")
+is.installed("knitr")
+is.installed("rmarkdown")
+is.installed("ggthemes")
+is.installed("xts")
+is.installed("cluster")
+is.installed("stats")
+is.installed("magrittr")
+is.installed("sp")
+is.installed("plotly")
+is.installed("maps")
+is.installed("nycflights13")
+
